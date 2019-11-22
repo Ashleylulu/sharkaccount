b3f88698163c3958d3df33125c69fbb06d135956
lumingyuan <lumingyuan@sunkfa.com>
2019-11-22 15:20:59 +0800

    lu

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,25 @@ export default {
 </script>
 
 <style>
+  body{
+    margin: 0;
+    padding: 0;
+  }
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  padding: 0;
+  margin: 0;
 }
+  .mint-header{
+    background: #F9D34C;
+    color: #000;
+    font-weight: bold;
+    font-size: 1rem;
+    height: 4.4rem;
+    padding-top: 1.4rem;
+  }
 </style>
--- src/pages/regist/regist.vue
+++ src/pages/regist/regist.vue
@@ -1,17 +1,30 @@
 <template>
-    <div>{{msg}}</div>
+    <div>
+        <mt-header title="注册">
+            <router-link to="/" slot="left">
+                <mt-button icon="back">返回</mt-button>
+            </router-link>
+        </mt-header>
+    </div>
 </template>
 
 <script>
+    import { Toast } from 'mint-ui';
+    import { Header } from 'mint-ui';
+    import { Tabbar, TabItem } from 'mint-ui';
     export default {
         data() {
             return {
                 msg: '注册',
             }
+        },
+        methods:{
+
         }
     }
 </script>
 
 <style scoped>
+
 
 </style>
